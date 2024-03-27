ddb57dd2c4dc62eb6179d7358524e63669525a53
John Assefa <johnassefaforbussines@gmail.com>
2024-03-27 16:00:42 -0700

    working on theleft button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -19,11 +20,18 @@ class HomePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.purple,
           leading: Container(
+            // ignore: prefer_const_constructors
             margin: EdgeInsets.all(10) ,
+            alignment: Alignment.center,
             decoration: BoxDecoration(
               color:Colors.white ,
               borderRadius: BorderRadius.circular(10)
-            ) 
+            ), 
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - Left 2.svg',
+              height: 20,
+              width: 20,
+              color: Colors.purple,)
           ),
       ) ,
     );
