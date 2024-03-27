3c0dcbe4166ca6e0de1ceed46e301e299e3d3cbd
John Assefa <johnassefaforbussines@gmail.com>
2024-03-27 15:49:21 -0700

    working on app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,1 +1,31 @@
 import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+          leading: Container(
+            margin: EdgeInsets.all(10) ,
+            decoration: BoxDecoration(
+              color:Colors.white ,
+              borderRadius: BorderRadius.circular(10)
+            ) 
+          ),
+      ) ,
+    );
+  }
+}
