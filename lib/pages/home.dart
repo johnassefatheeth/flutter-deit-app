5d7afddaec9961a5460975aa95b50ac763764804
John Assefa <johnassefaforbussines@gmail.com>
2024-03-27 16:15:06 -0700

    working on theleft button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,32 +8,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-          leading: Container(
-            // ignore: prefer_const_constructors
-            margin: EdgeInsets.all(10) ,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color:Colors.white ,
-              borderRadius: BorderRadius.circular(10)
-            ), 
-            child: SvgPicture.asset(
-              'assets/icons/Arrow - Left 2.svg',
-              height: 20,
-              width: 20,
-              color: Colors.purple,)
-          ),
-      ) ,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        leading: GestureDetector(
+          onTap: (){
+            
+          },
+          child: Container(
+          // ignore: prefer_const_constructors
+          margin: EdgeInsets.all(10) ,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color:Colors.white ,
+            borderRadius: BorderRadius.circular(10)
+          ), 
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+            color: Colors.purple,)
+        )),
+        actions: [
+          GestureDetector(
+            onTap: (){
+
+            },
+          child:Container(
+          // ignore: prefer_const_constructors
+          margin: EdgeInsets.all(10) ,
+          alignment: Alignment.center,
+          width: 37,
+          decoration: BoxDecoration(
+            color:Colors.white ,
+            borderRadius: BorderRadius.circular(10)
+          ), 
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            height: 5,
+            width: 5,
+            color: Colors.purple,)
+        )
+        )
+        ],
     );
   }
 }
