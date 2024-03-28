5d8c339e7e73ff36e09f639a87b5ca7a8c91d36f
John Assefa <johnassefaforbussines@gmail.com>
2024-03-28 14:02:27 -0700

    finishing

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -107,11 +107,8 @@ class _HomePageState extends State<HomePage> {
                         height: 30,
                         )
                         )
-
-                        
                       ],
                     ),
-
                   );
 
                 },
